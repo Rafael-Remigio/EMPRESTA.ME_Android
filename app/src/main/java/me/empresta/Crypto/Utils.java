f7c57bdc250a92a8e01fd0020e842d7db5a5e2b7
rafael <rafael.lixa@ua.pt>
2023-05-16 23:31:58 +0100

    Update Crypto functions

--- app/src/main/java/me/empresta/Crypto/Utils.java
+++ app/src/main/java/me/empresta/Crypto/Utils.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 public class Utils {
 
     public static final String algorithm = "SHA256withECDSA";
+    static org.bouncycastle.jce.spec.ECNamedCurveParameterSpec SPEC = org.bouncycastle.jce.ECNamedCurveTable.getParameterSpec("secp256r1");
 
     public static boolean verifySignature(byte[] publicKeyBytes,byte[] challenge,byte[] response) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException {
 
@@ -34,6 +35,116 @@ public class Utils {
     }
     private static ECParameterSpec params;
     private static final byte UNCOMPRESSED_POINT_INDICATOR = 0x04;
+
+
+    public static ECPublicKey fromUncompressedPoint(
+            final byte[] uncompressedPoint, final ECParameterSpec params)
+            throws Exception {
+        
+                System.out.println(uncompressedPoint.length);
+        int offset = 0;
+        if (uncompressedPoint[offset++] != UNCOMPRESSED_POINT_INDICATOR) {
+            throw new IllegalArgumentException(
+                    "Invalid uncompressedPoint encoding, no uncompressed point indicator");
+        }
+
+        int keySizeBytes = (params.getOrder().bitLength() + Byte.SIZE - 1)
+                / Byte.SIZE;
+
+        if (uncompressedPoint.length != 1 + 2 * keySizeBytes) {
+            throw new IllegalArgumentException(
+                    "Invalid uncompressedPoint encoding, not the correct size");
+        }
+
+        final BigInteger x = new BigInteger(1, Arrays.copyOfRange(
+                uncompressedPoint, offset, offset + keySizeBytes));
+        offset += keySizeBytes;
+        final BigInteger y = new BigInteger(1, Arrays.copyOfRange(
+                uncompressedPoint, offset, offset + keySizeBytes));
+        final java.security.spec.ECPoint w = new java.security.spec.ECPoint(x, y);
+        final ECPublicKeySpec ecPublicKeySpec = new ECPublicKeySpec(w, params);
+        final KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        ECPublicKey pubkey = (ECPublicKey) keyFactory.generatePublic(ecPublicKeySpec);
+        return pubkey;
+    }
+
+    public static byte[] toUncompressedPoint(final ECPublicKey publicKey) {
+
+        int keySizeBytes = (publicKey.getParams().getOrder().bitLength() + Byte.SIZE - 1)
+                / Byte.SIZE;
+
+        final byte[] uncompressedPoint = new byte[1 + 2 * keySizeBytes];
+        int offset = 0;
+        uncompressedPoint[offset++] = 0x04;
+
+        final byte[] x = publicKey.getW().getAffineX().toByteArray();
+        if (x.length <= keySizeBytes) {
+            System.arraycopy(x, 0, uncompressedPoint, offset + keySizeBytes
+                    - x.length, x.length);
+        } else if (x.length == keySizeBytes + 1 && x[0] == 0) {
+            System.arraycopy(x, 1, uncompressedPoint, offset, keySizeBytes);
+        } else {
+            throw new IllegalStateException("x value is too large");
+        }
+        offset += keySizeBytes;
+
+        final byte[] y = publicKey.getW().getAffineY().toByteArray();
+        if (y.length <= keySizeBytes) {
+            System.arraycopy(y, 0, uncompressedPoint, offset + keySizeBytes
+                    - y.length, y.length);
+        } else if (y.length == keySizeBytes + 1 && y[0] == 0) {
+            System.arraycopy(y, 1, uncompressedPoint, offset, keySizeBytes);
+        } else {
+            throw new IllegalStateException("y value is too large");
+        }
+        return uncompressedPoint;
+    }
+
+
+
+
+
+    static byte[] concat(byte[]...arrays)
+    {
+        // Determine the length of the result array
+        int totalLength = 0;
+        for (int i = 0; i < arrays.length; i++)
+        {
+            totalLength += arrays[i].length;
+        }
+    
+        // create the result array
+        byte[] result = new byte[totalLength];
+    
+        // copy the source arrays into the result array
+        int currentIndex = 0;
+        for (int i = 0; i < arrays.length; i++)
+        {
+            System.arraycopy(arrays[i], 0, result, currentIndex, arrays[i].length);
+            currentIndex += arrays[i].length;
+        }
+    
+        return result;
+    }
+
+
+
+    static byte[] uncompressedToCompressed(byte[] uncompKey){
+        org.bouncycastle.math.ec.ECPoint point = SPEC.getCurve().decodePoint(uncompKey);
+    
+    
+        return point.getEncoded(true);
+    
+    
+    }
+    
+     static byte[] compressedToUncompressed(byte[] compKey) {
+         org.bouncycastle.math.ec.ECPoint point = SPEC.getCurve().decodePoint(compKey);
+         byte[] x = point.getXCoord().getEncoded();
+         byte[] y = point.getYCoord().getEncoded();
+         // concat 0x04, x, and y, make sure x and y has 32-bytes:
+         return concat( concat(new byte[] {0x04},x), y);
+     }
 
 
 
